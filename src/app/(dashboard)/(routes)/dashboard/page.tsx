61cdf4d12026b1f2b727623582414911a1ee9d13
M Kamran <mkamran.4edu@gmail.com>
2024-06-19 16:44:05 +0500

    Add dashboard tools with icons and links

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,90 @@
+"use client";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import {
+  ArrowBigRight,
+  ArrowRight,
+  Code,
+  Image,
+  ImageIcon,
+  MessageSquare,
+  Music,
+  Video,
+} from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-export default async function Dashboard() {
-  return <div>Dashboard</div>;
+const tools = [
+  {
+    lable: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+  {
+    lable: "Music Generation",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/music",
+  },
+  {
+    lable: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: "/image",
+  },
+  {
+    lable: "Video Generation",
+    icon: Video,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: "/music",
+  },
+  {
+    lable: "Code Generation",
+    icon: Code,
+    color: "text-green-500",
+    bgColor: "bg-green-500/10",
+    href: "/music",
+  },
+];
+
+export default function Dashboard() {
+  const router = useRouter();
+  return (
+    <div className="mb-8">
+      <div className="mb-8 space-y-4">
+        <h1 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the Power of AI
+        </h1>
+        <p className="text-muted-foreground text-sm md:text-lg text-center font-light">
+          The Innovio is a platform for exploring the power of AI.
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => {
+              router.push(tool.href);
+            }}
+            key={tool.href}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)} />
+              </div>
+              <div className="font-semibold">{tool.lable}</div>
+            </div>
+            <div>
+              <ArrowRight className="w-5 h-5" />
+            </div>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 }
